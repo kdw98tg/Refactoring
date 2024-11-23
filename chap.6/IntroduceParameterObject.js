80908b31cd0c3dde1301b731c0b8c49ea017c8a0
kdw98tg <117254224+kdw98tg@users.noreply.github.com>
2024-11-23 23:44:43 +0900

    chap 6

---
+++ chap.6/IntroduceParameterObject.js
@@ -0,0 +1,16 @@
+function insertUser(name, age, phoneNumber, address) {
+    //insert 로직
+}
+
+function insertUser(user) {
+    //insert 로직
+}
+
+class User {
+    constructor(name, age, phoneNumber, address) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
